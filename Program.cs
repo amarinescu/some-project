26f54c42255a1567a29c677b15f7ee24c4710c40
Alexandru-Mihai Marinescu <Alexandru-Mihai.Marinescu@endava.com>
2023-04-03 09:42:01 +0300

    All the code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using SomeProject;
+
+int inactivityInterval = 2 * 60 * 1000;
+
+// Set the interval at which the mouse is clicked to 10 seconds
+int clickInterval = 10 * 1000;
+
+// Create a timer to check for inactivity
+Timer inactivityTimer = new Timer(CheckInactivity, null, 0, clickInterval);
+
+// Run the application until the user closes the console window
+while (true)
+{
+    Thread.Sleep(1000);
+}
+
+static void CheckInactivity(object state)
+{
+    // Get the time of the last user input
+    int idleTime = Win32.GetIdleTime();
+
+    // If the user has been inactive for 2 minutes, click the middle mouse button
+    if (idleTime > 2 * 60 * 1000)
+    {
+        Win32.keybd_event((byte)ConsoleKey.RightArrow, 0, 0, 0);
+        Win32.keybd_event((byte)ConsoleKey.RightArrow, 0, 2, 0);
+        Console.WriteLine("Prevented inactivity!");
+    }
+}
---
+++ Win32.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeProject
+{
+    static public class Win32
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public static int GetIdleTime()
+        {
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInputInfo);
+            GetLastInputInfo(ref lastInputInfo);
+            return ((int)Environment.TickCount - (int)lastInputInfo.dwTime);
+        }
+
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+    }
+}
